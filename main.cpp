153468d5aded1327a718f2fc7044ac4a79160342
AnselmTheCoder <omoyano97@gmail.com>
2018-04-11 14:14:58 +0200

    Restoration of files after the foo test
    Title is self-explanatory

--- main.cpp
+++ main.cpp
@@ -13,12 +13,6 @@
 static int prevTime;
 static Game game; // This object represents our whole game
 
-
-static void foo2()
-{
-	//Foo2 test too observe the changes in github files
-}
-
 // If a key is pressed this callback is called
 
 static void keyboardDownCallback(unsigned char key, int x, int y)
@@ -118,8 +112,5 @@ int main(int argc, char **argv)
 	return 0;
 }
 
-void foo() {
-
-}
 
 
